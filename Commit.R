e8c2aa50e8e0c65f20a5298e283b4944954d9bd3
kiliancarolan <32336995+kiliancarolan@users.noreply.github.com>
2017-11-18 15:26:26 +0000

    Access github
    Got github Api Key and accesed github

--- Commit.R
+++ Commit.R
@@ -4,3 +4,10 @@ intall.packages("httpuv")
 library(httpuv)
 instal.packages("httr")
 library(httr)
+oauth_endpoints("github")
+myapp <- oauth_app(appname = "Software_Engineering_Kilian_Carolan",
+                   key = "237cd284e97e7cb3d561",
+                   secret = "a00444a548f2cf7d81d040c90cb389c0be3bf814")
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
