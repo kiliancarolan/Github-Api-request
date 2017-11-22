c83f40303ac0ca0b3756a61a8cf31f51d490ddd1
kiliancarolan <32336995+kiliancarolan@users.noreply.github.com>
2017-11-22 16:27:18 +0000

    Pagnation
    Coded to allow multiple json pages to be combined into one list. need a method which will allow the total number of json pages to be used easily

--- Apirequest.r
+++ Apirequest.r
@@ -22,9 +22,31 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 gitDF[gitDF$full_name == "phadej", "created_at"] 
 gitDF
 gg_commits<-fromJSON("https://api.github.com/repos/hadley/ggplot2/commits")
-phadejRaw<-("https://api.github.com/users/phadej/followers?per_page=100;")
-data<-fromJSON(phadejRaw)
-length(data)
-final_data<-do.call(rbind,data)
-write.csv(final_data,"final_data.csv")
-head(final_data)
+phadejRaw<-("https://api.github.com/users/phadej/followers")
+pages<-list()
+mydata1<-fromJSON(paste0(phadejRaw, "?page=1"),flatten=TRUE)
+mydata2<-fromJSON(paste0(phadejRaw, "?page=2"),flatten=TRUE)
+mydata3<-fromJSON(paste0(phadejRaw, "?page=3"),flatten=TRUE)
+mydata4<-fromJSON(paste0(phadejRaw, "?page=4"),flatten=TRUE)
+mydata5<-fromJSON(paste0(phadejRaw, "?page=5"),flatten=TRUE)
+mydata6<-fromJSON(paste0(phadejRaw, "?page=6"),flatten=TRUE)
+
+nrow(mydata1)
+finalDataTest<-rbind_pages(list(mydata1,mydata2,mydata3,mydata4,mydata5,mydata6))
+finalDataTest
+for(i in 1:6)
+{
+  mydata<-fromJSON(paste0(phadejRaw, "?page=",i))
+  message("Retrieving page ", i)
+  pages[[i+1]] <- mydata
+}
+logins <- rbind_pages(pages)
+#check
+nrow(logins)
+
+#data<-fromJSON(phadejRaw)
+#length(data)
+#final_data<-do.call(rbind,data)
+#write.csv(final_data,"final_data.csv")
+#head(final_data)
+
