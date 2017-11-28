0b08889a00f43731a9452005b8089e4402d8d781
kiliancarolan <32336995+kiliancarolan@users.noreply.github.com>
2017-11-28 17:26:46 +0000

    Streamlined Pagnation
    Streamlined previous code

--- Apirequest.r
+++ Apirequest.r
@@ -7,46 +7,26 @@ myapp <- oauth_app(appname = "Software_Engineering_Kilian_Carolan",
                    secret = "a00444a548f2cf7d81d040c90cb389c0be3bf814")
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 gtoken <- config(token = github_token)
-phadejReq <- GET("https://api.github.com/users/phadej/followers?per_page=100;", gtoken)
-
-# Take action on http error
-stop_for_status(phadejReq)
-
-# Extract content from a request
-json1 = content(phadejReq)
-
-# Convert to a data.frame
-gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
-
-# Subset data.frame
-gitDF[gitDF$full_name == "phadej", "created_at"] 
-gitDF
-gg_commits<-fromJSON("https://api.github.com/repos/hadley/ggplot2/commits")
-phadejRaw<-("https://api.github.com/users/phadej/followers")
+usersTest<-("https://api.github.com/users")
 pages<-list()
-mydata1<-fromJSON(paste0(phadejRaw, "?page=1"),flatten=TRUE)
-mydata2<-fromJSON(paste0(phadejRaw, "?page=2"),flatten=TRUE)
-mydata3<-fromJSON(paste0(phadejRaw, "?page=3"),flatten=TRUE)
-mydata4<-fromJSON(paste0(phadejRaw, "?page=4"),flatten=TRUE)
-mydata5<-fromJSON(paste0(phadejRaw, "?page=5"),flatten=TRUE)
-mydata6<-fromJSON(paste0(phadejRaw, "?page=6"),flatten=TRUE)
-
-nrow(mydata1)
-finalDataTest<-rbind_pages(list(mydata1,mydata2,mydata3,mydata4,mydata5,mydata6))
-finalDataTest
-for(i in 1:6)
+for(i in 1:2)
 {
-  mydata<-fromJSON(paste0(phadejRaw, "?page=",i))
+  mydataPaste<-paste0(usersTest, "?per_page=75&page=",i)
+  mydata<-GET(mydataPaste,gtoken)
+  json1 = content(mydata)
+  mydataFrame = jsonlite::fromJSON(jsonlite::toJSON(json1))
   message("Retrieving page ", i)
-  pages[[i+1]] <- mydata
+  pages[[i+1]] <- mydataFrame
+  logins <- rbind_pages(pages)
 }
-logins <- rbind_pages(pages)
+
+#logins <- rbind_pages(pages)
+logins$avatar_url<- NULL
+logins$gravatar_id<-NULL
+for(i)
 #check
 nrow(logins)
-
-#data<-fromJSON(phadejRaw)
-#length(data)
-#final_data<-do.call(rbind,data)
+length(logins)
+#final_data<-do.call(rbind,logins)
 #write.csv(final_data,"final_data.csv")
 #head(final_data)
-
