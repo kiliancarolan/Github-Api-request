d31d8f4ea2e9888276f27a89b39e5cbef93aeb9d
kiliancarolan <32336995+kiliancarolan@users.noreply.github.com>
2017-12-06 14:33:49 +0000

    Commented code
    Commented out code, installed new package and decluttered

--- Apirequest.r
+++ Apirequest.r
@@ -1,18 +1,26 @@
+###Package Instalation, some will not be necessary but allow for future adaption
 library(jsonlite)
 library(httpuv)
 library(httr)
-library(plyr)
+library(ggplot2)
+library(d3r)
+
+### getting oauth to allow for more api requests within given time period
 oauth_endpoints("github")
 myapp <- oauth_app(appname = "Software_Engineering_Kilian_Carolan",
                    key = "237cd284e97e7cb3d561",
                    secret = "a00444a548f2cf7d81d040c90cb389c0be3bf814")
 github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 gtoken <- config(token = github_token)
-usersTest<-("https://api.github.com/users")
+
+###BaseUrl
+usersUrl<-("https://api.github.com/users")
+
+###Allow for pagnation
 pages<-list()
 for(i in 1:2)
 {
-  mydataPaste<-paste0(usersTest, "?per_page=75&page=",i)
+  mydataPaste<-paste0(usersUrl, "?per_page=75&page=",i)
   mydata<-GET(mydataPaste,gtoken)
   json1 = content(mydata)
   mydataFrame = jsonlite::fromJSON(jsonlite::toJSON(json1))
@@ -20,34 +28,35 @@ for(i in 1:2)
   pages[[i+1]] <- mydataFrame
   logins <- rbind_pages(pages)
 }
-loginsName<-logins$login
-loginsName
-loginsName[3]
-##Test Code to get number of followers from sample of users
-testPaste<-paste0(usersTest,"/",loginsName[3])
-testData=GET(testPaste,gtoken)
-jsonTest=content(testData)
-testDF=jsonlite::fromJSON(jsonlite::toJSON(jsonTest))
-testDF
-testDF$followers
+### Following code will produce a graph of users Followers Vs Following Numbers 
+### given sample size of 70
+followinglist<-c()
+followerlist<-c()
 i=1
 
-  testLoopPaste<-paste0(usersTest,"/",loginsName[i])
+while(i<70)
+{
+  testLoopPaste<-paste0(usersUrl,"/",loginsName[i])
   testLoopData=GET(testLoopPaste,gtoken)
   jsonLoopTest=content(testLoopData)
   testLoopDF=jsonlite::fromJSON(jsonlite::toJSON(jsonLoopTest))
+  testFollower<-testLoopDF$followers
+  testFollowing<-testLoopDF$following
   print(paste("name of user is ",loginsName[i]," number of followers ", testLoopDF$followers,
               " They follow ",testLoopDF$following, "people"))
-  plot(testLoopDF$followers,testLoopDF$following, type=
-       "b",main="Followers vs following",xlab="Followers",ylab="Following")
+  followinglist=c(followinglist,testFollowing)
+  followerlist<-c(followerlist,testFollower)
+  
   i=i+1
+}
+plot(followinglist,followerlist)
 
-###Show testusers followers details
+###Show users followers details
 
 i=1
 while(i<3)
 {
-  followerPaste<-paste0(usersTest,"/",loginsName[i],"/followers")
+  followerPaste<-paste0(usersUrl,"/",loginsName[i],"/followers")
   followerData=GET(followerPaste,gtoken)
   jsonfollower=content(followerData)
   followerDF=jsonlite::fromJSON(jsonlite::toJSON(jsonfollower))
@@ -56,13 +65,4 @@ while(i<3)
   print(followerNames)
   i=i+1
 }
-#logins <- rbind_pages(pages)
-logins$avatar_url<- NULL
-logins$gravatar_id<-NULL
 
-#check
-nrow(logins)
-length(logins)
-#final_data<-do.call(rbind,logins)
-#write.csv(final_data,"final_data.csv")
-#head(final_data)
